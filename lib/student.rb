fc9d70ff51ccef3126140f6abea0ce74b88a2f4d
github username <github email address>
2020-03-19 02:27:36 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,16 +3,39 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    student = Student.new
+    id, name, grade = row
+    student.id = id
+    student.name = name
+    student.grade = grade
+    student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students;
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
   
   def save
@@ -39,5 +62,31 @@ class Student
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
+  end
+
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  def self.all_students_in_grade_9
+    self.all_students_in_grade_X(9)
+  end
+
+  def self.first_X_students_in_grade_10(x)
+    self.all_students_in_grade_X(10).slice(0, x)
+  end
+
+  def self.students_below_12th_grade
+    self.all.select {|x| x.grade.to_i < 12 }
+  end
+
+  def self.first_student_in_grade_10
+    self.all_students_in_grade_X(10).first
   end
 end
